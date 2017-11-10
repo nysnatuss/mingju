3ceaf7278fdb47b14c0a94b250274849ead35fc0
nys <33534144+nysnatuss@users.noreply.github.com>
2017-11-10 10:33:19 +0800

    init

---
+++ mingju.js
@@ -0,0 +1,142 @@
+// Variables
+var data_arr = [];
+var x1;
+var x2;
+var selmj = [];
+var question;
+var answer;
+var limit;
+var currQuestion;
+/* TODO:
+var wrongLimit = 3;
+var timeout = 60; // seconds
+var ansSide = [1,1]; // ??
+*/
+// Document ready
+$(document).ready(function() {
+	dataHandling();
+});
+
+// Convenient
+function mjLoop(x) {
+	var a;
+	switch (x) {
+		case 0:
+			return 'ybb';
+			break;
+		case 1:
+			return 'z1';
+			break;
+		case 2:
+			return 'z2';
+			break;
+		case 3:
+			return 'z3';
+			break;
+		case 4:
+			return 'z4';
+			break;
+		case 5:
+			return 'z5';
+			break;
+		default:
+			alert("Error caught! (mjl1)\nPlease report to the website administrator with reproduce steps.");
+			return '';
+			break;
+	}
+}
+
+// Setup
+function intro() {
+	// Check for errors in input
+	var selectAll = 0;
+	for (var x=0; x<=5; x++) {
+		if (document.getElementById(mjLoop(x)).checked) {
+			// If selectAll < 1, that means there are one is selected
+			selectAll = selectAll + 1;
+		}
+	}
+	if (selectAll < 1) {
+		alert("您没有选择你所要测验的名句！");
+	} else if (parseInt($("#mjNum").val()) < 1 ||
+			   parseInt($("#mjNum").val()) > 1000) {
+		alert("请您选择正确的测验次数！");
+	} else {
+		for (var x=0; x<=5; x++) {
+			if (document.getElementById(mjLoop(x)).checked) {
+				selmj[x] = 1;
+			} else {
+				selmj[x] = 0;
+			}
+		}
+		limit = parseInt($("#mjNum").val());
+		$("#intro2").fadeOut("slow");
+		$("#intro").fadeOut("slow");
+		$("#qDisplay").delay(15000).css("display", "block");
+		currQuestion = 0;
+		display();
+	}
+}
+
+function display() {
+	currQuestion = currQuestion + 1;
+	var x1;
+	do {
+		x1 = Math.floor(Math.random() * data_arr.length);
+	} while (selmj[x1] !== 0);
+	var x2 = Math.floor(Math.random() * data_arr[x1].length);
+	question = data_arr[0][0].replace(/ *\[[^\]]*]/, ''); // rm anything in []
+	answer = data_arr[0][0].match(/\[(.*?)\]/)[1].replace(/[，。；？,.;?]/g, ''); // rm anything other than [] and rm symbol
+	if (question.startsWith("，") || question.startsWith("；")) {
+		$('#after').text(question + "。");
+	} else {
+		$('#before').text(question);
+	}
+	$('#count').text(currQuestion);
+	switch (x1) {
+		case 0:
+			$('#level').text("预备班");
+			break;
+		case 1:
+			$('#level').text("中一");
+			break;
+		case 2:
+			$('#level').text("中二");
+			break;
+		case 3:
+			$('#level').text("中三");
+			break;
+		case 4:
+			$('#level').text("中四");
+			break;
+		case 5:
+			$('#level').text("中五");
+			break;
+	}
+}
+
+function check() {
+	var userAns = $("#input1").val();
+	userAns = userAns.replace(/[，。；？,.;?]/g, '');
+	if ($("#input1").val() === "" ||
+		$("#input1").val() === undefined) {
+		alert("您没有输入任何字母！");
+	} else if (userAns == answer) {
+		// TODO: Beautify
+		alert("您答对了！");
+	} else {
+		// TODO: Beautify
+		alert("您答错了！");
+	}
+}
+
+// Data handling
+function dataHandling() {
+	for (var x=0;x<=1;x++) { // Change for z4,z5
+		var txtFile = 'list/' + mjLoop(x) + '.txt';
+		$.get(txtFile, function(data) {
+			var lines = data.split('\n');
+			data_arr.push(lines);
+		}, 'text');
+	}
+}
